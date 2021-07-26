25f3947c93c5a97556f6bf3c473b494ce8aa359a
gabrielbeaupre <72994398+gabrielbeaupre@users.noreply.github.com>
2021-07-26 08:27:52 -0700

    Add files via upload

---
+++ src/Navigation_bar.js
@@ -0,0 +1,12 @@
+function Navigation_bar(){
+    return(
+        
+        <div className="navigation_bar">
+            <a href="../public/index.html">Accueil</a>
+            <a href="">Article du jour</a>
+            <a href="../public/aide.html">Aide</a>
+        </div>
+    );
+}
+
+export default Navigation_bar;
